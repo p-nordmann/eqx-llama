941785578fb630003af9efe122dfc898d3ec9d34
p-nordmann <pierrelouis.nordmann@gmail.com>
2024-10-18 20:31:23 +0000

    feat: added new attention module

---
+++ eqx_llama/llama_attention.py
@@ -0,0 +1,118 @@
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+from beartype import beartype
+from jaxtyping import Array, Float32, PRNGKeyArray, jaxtyped
+
+from .llama_config import LLaMAConfig
+from .normalization import RMSLayerNorm
+
+
+class AttentionModule(eqx.Module):
+    norm: RMSLayerNorm
+    rope_embeddings: eqx.nn.RotaryPositionalEmbedding
+    linear_q: eqx.nn.Linear
+    linear_k: eqx.nn.Linear
+    linear_v: eqx.nn.Linear
+    linear_o: eqx.nn.Linear
+
+    num_attention_heads: int = eqx.field(static=True)
+    size_attention_heads: int = eqx.field(static=True)
+
+    def __init__(
+        self,
+        config: LLaMAConfig,
+        *,
+        key: PRNGKeyArray,
+    ):
+        assert (
+            config.num_attention_heads * config.size_attention_heads
+            == config.size_layer
+        )
+        self.num_attention_heads = config.num_attention_heads
+        self.size_attention_heads = config.size_attention_heads
+
+        self.norm = RMSLayerNorm(config.size_layer)
+
+        self.rope_embeddings = eqx.nn.RotaryPositionalEmbedding(
+            config.size_attention_heads, 10_000
+        )
+
+        key_linear, key = jax.random.split(key)
+        self.linear_q = eqx.nn.Linear(
+            config.size_layer,
+            config.size_layer,
+            use_bias=False,
+            key=key_linear,
+        )
+
+        key_linear, key = jax.random.split(key)
+        self.linear_k = eqx.nn.Linear(
+            config.size_layer,
+            config.size_layer,
+            use_bias=False,
+            key=key_linear,
+        )
+
+        key_linear, key = jax.random.split(key)
+        self.linear_v = eqx.nn.Linear(
+            config.size_layer,
+            config.size_layer,
+            use_bias=False,
+            key=key_linear,
+        )
+
+        key_linear, key = jax.random.split(key)
+        self.linear_o = eqx.nn.Linear(
+            config.size_layer,
+            config.size_layer,
+            use_bias=False,
+            key=key_linear,
+        )
+
+    def _compute_embeddings(
+        self,
+        xs: Float32[Array, " seq_len size_layer"],
+        linear: eqx.nn.Linear,
+        use_position_embeddings: bool = False,
+    ) -> Float32[Array, " seq_len num_heads size_heads"]:
+        projected_xs = jax.vmap(linear)(xs)
+        hs = jnp.reshape(
+            projected_xs,
+            newshape=(-1, self.num_attention_heads, self.size_attention_heads),
+        )
+        if not use_position_embeddings:
+            return hs
+        return jax.vmap(self.rope_embeddings, in_axes=1, out_axes=1)(hs)
+
+    @jaxtyped(typechecker=beartype)
+    def __call__(
+        self,
+        xs: Float32[Array, " seq_len size_layer"],
+        enable_dropout: bool = False,
+        key: PRNGKeyArray | None = None,
+    ) -> Float32[Array, " seq_len size_layer"]:
+        xs_normalized = jax.vmap(self.norm)(xs)
+        qs = self._compute_embeddings(
+            xs_normalized,
+            self.linear_q,
+            use_position_embeddings=True,
+        )
+        ks = self._compute_embeddings(
+            xs_normalized,
+            self.linear_k,
+            use_position_embeddings=True,
+        )
+        vs = self._compute_embeddings(xs_normalized, self.linear_v)
+        attention_out = compute_self_attention(qs, ks, vs)
+        return jax.vmap(self.linear_o)(jax.lax.collapse(attention_out, 1, 3))
+
+
+def compute_self_attention(
+    qs: Float32[Array, " seqlen num_heads head_dim"],
+    ks: Float32[Array, " seqlen num_heads head_dim"],
+    vs: Float32[Array, " seqlen num_heads head_dim"],
+) -> Float32[Array, " seqlen num_heads head_dim"]:
+    return jax.nn.dot_product_attention(
+        qs, ks, vs, is_causal=True, implementation="xla"
+    )
