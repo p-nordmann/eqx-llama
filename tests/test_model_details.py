7f42ae292d391304511fb2301292c6ae0b267901
Pierre-Louis Nordmann <pierrelouis.nordmann@gmail.com>
2025-07-20 11:28:35 +0200

    chore: added a few tests for the pallas implementation
    Note: on GPU, the pallas tests fail.

--- tests/test_model_details.py
+++ tests/test_model_details.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 
+from eqx_llama.llama_attention import compute_self_attention
 from eqx_llama.utils import RMSLayerNorm, apply_rotary_embeddings
 
 
@@ -54,3 +55,49 @@ def test_apply_rotary_embeddings():
     )
     print(got, want, sep="\n")
     assert jnp.allclose(got, want, atol=eps).item()
+
+
+def test_apply_rotary_embeddings_with_start_index():
+    eps = 1e-8
+
+    key = jax.random.PRNGKey(0)
+    xs = jax.random.normal(key, (128, 16))
+    ys = apply_rotary_embeddings(xs)
+
+    for k in range(0, 128, 16):
+        zs = apply_rotary_embeddings(xs[k:], start_index=k)
+        assert jnp.allclose(zs, ys[k:], atol=eps).item()
+
+
+def test_pallas_self_attention():
+    eps = 1e-6
+
+    kq, kk, kv = jax.random.split(jax.random.PRNGKey(0), 3)
+    qs = jax.random.normal(kq, (32, 8, 16))
+    ks = jax.random.normal(kq, (128, 8, 16))
+    vs = jax.random.normal(kq, (128, 8, 16))
+
+    out_xla = compute_self_attention(qs, ks, vs, attn_implementation="xla")
+    out_pallas = compute_self_attention(
+        qs, ks, vs, attn_implementation="pallas", interpret=True
+    )
+
+    assert jnp.allclose(out_pallas, out_xla, atol=eps)
+
+
+def test_pallas_self_attention_backward():
+    eps = 1e-6
+
+    kq, kk, kv = jax.random.split(jax.random.PRNGKey(0), 3)
+    qs = jax.random.normal(kq, (4, 4, 8))
+    ks = jax.random.normal(kq, (8, 4, 8))
+    vs = jax.random.normal(kq, (8, 4, 8))
+
+    out_xla = jax.jacobian(compute_self_attention)(
+        qs, ks, vs, attn_implementation="xla"
+    )
+    out_pallas = jax.jacobian(compute_self_attention)(
+        qs, ks, vs, attn_implementation="pallas", interpret=True
+    )
+
+    assert jnp.allclose(out_pallas, out_xla, atol=eps)
