f9a22237dae7b636b56666f432d8e6c21035a445
Pierre-Louis Nordmann <pierrelouis.nordmann@gmail.com>
2025-08-14 16:50:55 +0200

    chore: updated annotations for rms layer norm

--- eqx_llama/utils.py
+++ eqx_llama/utils.py
@@ -22,7 +22,7 @@ class RMSLayerNorm(eqx.Module):
     Known to give similar results to layer norm, with reduced compute.
     """
 
-    weight: Float[Array, " dim"]
+    weight: Float[Array, "dim"]
     eps: float = eqx.field(static=True)
 
     def __init__(self, dim: int, eps: float = 1e-6, **kwargs):
@@ -30,7 +30,7 @@ class RMSLayerNorm(eqx.Module):
         self.weight = jnp.ones(shape=(dim,))
         self.eps = eps
 
-    def __call__(self, x: Float[Array, " dim"]) -> Float[Array, " dim"]:
+    def __call__(self, x: Float[Array, "... dim"]) -> Float[Array, "... dim"]:
         moment_2 = jnp.mean(x.astype(self.weight.dtype) ** 2, axis=-1, keepdims=True)
         x_normed = x * jax.lax.rsqrt(moment_2 + self.eps)
         return (self.weight * x_normed).astype(x.dtype)
